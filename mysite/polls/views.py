9f74fc68c2b03d7d8e9b054cd0ef1d3144a2a30c
thevishaal <thevishaal0@gmail.com>
2025-12-31 21:42:17 +0530

    static folder, generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 # from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -23,10 +24,10 @@ from .models import Question, Choice
 #     return HttpResponse(template.render(context, request))
 
 # shortcut method for render template
-def index(request):
+'''def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    return render(request, "polls/index.html", context)'''
 
 # Raising a 404 error
 # def detail(request, question_id):
@@ -37,13 +38,13 @@ def index(request):
 #     return render(request, "polls/detail.html", {"question": question})
 
 # a shorcut :  get_object_or_404()
-def detail(request, question_id):
+'''def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question},)
+    return render(request, "polls/results.html", {"question": question},)'''
 
 
 def vote(request, question_id):
@@ -57,3 +58,20 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+# Use generic views: Less code is better
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
