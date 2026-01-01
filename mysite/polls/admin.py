b9ae1e6bb665f746f9a6ad6121fa6a4c27af6fec
thevishaal <thevishaal0@gmail.com>
2026-01-01 06:42:24 +0530

    admin form customize

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,5 +5,23 @@ from .models import Question, Choice
 # admin.site.index_title = "Amazing Title"
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+# customize admin field
+'''class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]'''
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Add New Question", {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
